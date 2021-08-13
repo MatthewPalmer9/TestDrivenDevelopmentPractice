f552ee6e82f78216483cddfddd3ce869216d0f6e
Matthew Palmer <matthewpalmer9@outlook.com>
2021-08-13 11:30:41 -0500

    Grouped all numerical tests under a single descriptive test.

--- test.mjs
+++ test.mjs
@@ -17,28 +17,30 @@ function add(num1, num2) {
 const EVEN_NUMBER = 4;
 const ODD_NUMBER = 3;
 
-describe('isEven', () => {
-    it('should return true when number is even', () => {
-        isEven(EVEN_NUMBER).should.be.true;
+describe('Numerical Tests', () => {
+    describe('isEven', () => {
+        it('should return true when number is even', () => {
+            isEven(EVEN_NUMBER).should.be.true;
+        });
+    
+        it('should return false when number is odd', () => {
+            isEven(ODD_NUMBER).should.be.false;
+        });
     });
-
-    it('should return false when number is odd', () => {
-        isEven(ODD_NUMBER).should.be.false;
-    });
-});
-
-describe('add without setup/teardown', () => {
-    let num;
-    beforeEach(() => { // sets num to 5 before each test is run
-        num = 5;
-    });
-
-    it('should be ten whtn adding 5 to 5', () => {
-        num = add(num, 5);
-        num.should.equal(10);
-    });
-
-    it('should be twelve when adding 7 to 5', () => {
-        add(num, 7).should.equal(12);
+    
+    describe('add without setup/teardown', () => {
+        let num;
+        beforeEach(() => { // sets num to 5 before each test is run
+            num = 5;
+        });
+    
+        it('should be ten when adding 5 to 5', () => {
+            num = add(num, 5);
+            num.should.equal(10);
+        });
+    
+        it('should be twelve when adding 7 to 5', () => {
+            add(num, 7).should.equal(12);
+        });
     });
 });
